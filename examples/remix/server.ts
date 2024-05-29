44b476ffa62049a5e5b656b9733a67c0867b5cfd
Sunil Pai <spai@cloudflare.com>
2024-05-29 23:55:05 +0100

    Update server.ts

--- examples/remix/server.ts
+++ examples/remix/server.ts
@@ -1,6 +1,8 @@
 import { createRequestHandler, logDevReady } from "@remix-run/cloudflare";
 import * as build from "@remix-run/dev/server-build";
 
+// @ts-expect-error process isn't a global yet
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 if (process.env.NODE_ENV === "development") {
   logDevReady(build);
 }
