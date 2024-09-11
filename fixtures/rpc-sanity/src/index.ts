caadef8f5280fab328459bfa3bd193572310e7d7
Sunil Pai <spai@cloudflare.com>
2024-09-11 18:00:03 +0100

    Update index.ts

--- fixtures/rpc-sanity/src/index.ts
+++ fixtures/rpc-sanity/src/index.ts
@@ -9,14 +9,26 @@ export class MyServer extends Server<Env> {
   async testMethod() {
     return this.name;
   }
+  onRequest(request: Request): Response | Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname !== "/test") {
+      throw new Error("test onRequest");
+    }
+    return new Response("test onRequest");
+  }
 }
 
 const SESSION_ID = "session-id";
 
 export default {
   async fetch(request: Request, env: Env, _ctx: ExecutionContext) {
+    const url = new URL(request.url);
     const stub = await getServerByName(env.MyServer, SESSION_ID);
-    const value = await stub.testMethod();
-    return new Response(`the value is ${value}`);
+
+    if (url.pathname === "/rpc") {
+      const value = await stub.testMethod();
+      return new Response(`the value is ${value}`);
+    }
+    return stub.fetch(request);
   }
 } satisfies ExportedHandler<Env>;
