4db73c4366279a1b7b0fdc4fb8e733ed9250cedb
Sunil Pai <spai@cloudflare.com>
2025-02-12 10:18:04 +0000

    tldraw fixture on vite

---
+++ fixtures/tldraw/vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+export default defineConfig({ plugins: [cloudflare(), react()] });
