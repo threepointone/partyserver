c714bc88a12a216bb9c588f08d0a7a0db877eae2
Kevin Kipp <kkipp@cloudflare.com>
2025-05-29 16:30:11 -0500

    Add easy TURN server support

---
+++ .changeset/polite-doors-decide.md
@@ -0,0 +1,5 @@
+---
+"partytracks": patch
+---
+
+Add easy TURN server support
--- fixtures/partytracks/src/server/index.ts
+++ fixtures/partytracks/src/server/index.ts
@@ -2,16 +2,20 @@ import { Hono } from "hono";
 import { routePartyTracksRequest } from "partytracks/server";
 
 type Bindings = {
-  CALLS_APP_ID: string;
-  CALLS_APP_TOKEN: string;
+  REALTIME_SFU_APP_ID: string;
+  REALTIME_SFU_APP_TOKEN: string;
+  REALTIME_TURN_SERVER_APP_ID: string;
+  REALTIME_TURN_SERVER_APP_TOKEN: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.all("/partytracks/*", (c) =>
   routePartyTracksRequest({
-    appId: c.env.CALLS_APP_ID,
-    token: c.env.CALLS_APP_TOKEN,
+    appId: c.env.REALTIME_SFU_APP_ID,
+    token: c.env.REALTIME_SFU_APP_TOKEN,
+    turnServerAppId: c.env.REALTIME_TURN_SERVER_APP_ID,
+    turnServerAppToken: c.env.REALTIME_TURN_SERVER_APP_TOKEN,
     request: c.req.raw
   })
 );
